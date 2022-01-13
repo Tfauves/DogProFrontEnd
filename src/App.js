736d7edf1d7c7c5018865163a9229f5480237907
Tfauves <tfauvelle@gmail.com>
2022-01-12 22:05:22 -0500

    need to check path for profile.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-
 import './App.css';
-import Button from './components/common/Button';
+import {BrowserRouter} from "react-router-dom"
+import AppRouter from './components/Routers/AppRouter';
+import {AuthProvider} from './components/Providers/AuthProvider';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {fas} from "@fortawesome/free-solid-svg-icons";
 
 
+library.add(fas);
 
 function App() {
+
   return (
-    <div className="App">
-      <Button />
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        
+          <AppRouter />
+       
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
