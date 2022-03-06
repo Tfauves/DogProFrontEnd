6f75a008f5168a0588eece0ca9afa01fc7e9e076
Tfauves <tfauvelle@gmail.com>
2022-03-06 14:25:31 -0500

    bootstrap and landing img added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,31 +1,52 @@
-import React from 'react';
-import { Fragment } from 'react/cjs/react.production.min';
-import image from '../../assets/landingPage.jpg'
+import React from "react";
+import image from "../../assets/landingPage.jpg";
+
 const Home = () => {
+  //   const displayLandingImg = () => {
+  //     return(
+  //         <div style={{
+  //             backgroundImage: `url(${image})`,
+  //             backgroundSize: 'cover',
+  //             height: '27vh',
+  //             display: 'flex',
+  //             flexDirection: 'row',
+  //             margin: '1rem 0rem',
+  //             padding: '8px',
+  //             maxWidth: '900px',
+  //             width: '100%'
+  //         }}>
 
-  // const [count, setCount] = useState(0);
+  //         </div>
 
-  // const handleClick = () => {
-  //   setCount(count + 1);
-  // } 
-
-  // const handleReduce = () => {
-  //   setCount(count - 1);
+  //     )
   // }
 
   return (
-    <div style={{
-      display: "flex",
-      flex: "1",
-      flexDirection: "column",
-      alignItems: 'center',
-      minHeight: '100vh',
-    }}>
-      
+    <div
+      style={{
+        display: "flex",
+        flex: "1",
+        flexDirection: "column",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
       <h1>Home</h1>
-   
+      <div
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: "cover",
+          height: "50vh",
+          display: "flex",
+          flexDirection: "row",
+          margin: "1rem 0rem",
+          padding: "8px",
+          maxWidth: "900px",
+          width: "100%",
+        }}
+      ></div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
