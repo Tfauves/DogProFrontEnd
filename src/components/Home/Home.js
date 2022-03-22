0b854cce0d80dd045d9eb8e59761e996453525ed
Tranquility Base <tfauvelle@gmail.com>
2022-03-22 15:54:07 -0400

    login screen typo fixed and centered title

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,17 @@
 import React from "react";
 import splashImage from "../../assets/splash.jpg";
-// import Container from "../common/Container";
+import Container from "../common/Container";
 import Splash from "../common/Splash";
-import { Container } from "react-bootstrap";
+
 
 const Home = () => {
   return (
-    <Container fluid>
+    <Container>
       <Splash image={splashImage} style={{ color: "F1F1F1" }}>
-        <h1 style={{}}>Lorem Ipsum</h1>
-        <h2 style={{}}>do eiusmod tempor incididunt ut labore </h2>
-
+        <h1 style={{textAlign: "center"}}>Lorem Ipsum</h1>
+        <h2 style={{textAlign: "center"}}>do eiusmod tempor incididunt ut labore </h2>
         </Splash>
     </Container>
-
-
   );
 };
 
