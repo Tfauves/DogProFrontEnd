476b80e1917e1d43d99c9d983fc67f2307308090
Tranquility Base <tfauvelle@gmail.com>
2022-03-26 14:39:20 -0400

    splash screen

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,9 +7,9 @@ import Splash from "../common/Splash";
 const Home = () => {
   return (
     <Container>
-      <Splash image={splashImage} style={{ color: "F1F1F1" }}>
-        <h1 style={{fontFamily: 'Poppins, sans-serif', marginTop: "3em", textAlign: "center" }}>For the love of Dog</h1>
-        <h2 style={{fontFamily: 'Oswald, sans-serif', marginTop: "1em", textAlign: "center" }}>
+      <Splash image={splashImage} style={{ color: "#f1f1f1" }}>
+        <h1 style={{fontFamily: 'Poppins, sans-serif', marginTop: "3em", textAlign: "center", fontSize: "5em", textShadow: '0 0 6px #303030, 0 0 5px #303030' }}>For the love of Dog</h1>
+        <h2 style={{fontFamily: 'Oswald, sans-serif', marginTop: "1em", textAlign: "center", textShadow: '0 0 6px #303030, 0 0 5px #303030' }}>
           Balance. Friendship. Loyalty
         </h2>
       </Splash>
