495f1d433cfe8bb2233b4d84162377a5d75a47bc
Tranquility Base <tfauvelle@gmail.com>
2022-03-28 18:17:11 -0400

    splash screen overlay

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,21 @@
 import React from "react";
-import splashImage from "../../assets/splash.jpg";
+import splashOverlay from "../../assets/splashOverlay.jpg";
 import Container from "../common/Container";
 import Splash from "../common/Splash";
-import Overlay from "../Overlay";
 
 const Home = () => {
   return (
     <Container>
-      <Splash image={splashImage} style={{ color: "#f1f1f1" }}>
-        <h1 style={{fontFamily: 'Poppins, sans-serif', marginTop: "3em", textAlign: "center", fontSize: "5em", textShadow: '0 0 6px #303030, 0 0 5px #303030' }}>For the love of Dog</h1>
-        <h2 style={{fontFamily: 'Oswald, sans-serif', marginTop: "1em", textAlign: "center", textShadow: '0 0 6px #303030, 0 0 5px #303030' }}>
-          Balance. Friendship. Loyalty
-        </h2>
+      <Splash image={splashOverlay} style={{ color: "#f1f1f1" }}>
+        <h1 style={{fontFamily: 'Bebas Neue, cursive',
+        marginTop: "3em", 
+        textAlign: "center", 
+        fontSize: "5em", 
+        textShadow: '0 0 6px #303030, 0 0 5px #303030' }}>For the love of Dog</h1>
+        <h2 style={{fontFamily: 'Oswald, sans-serif', 
+        marginTop: "1em", 
+        textAlign: "center", 
+        textShadow: '0 0 6px #303030, 0 0 5px #303030' }}>Balance. Friendship. Loyalty</h2>
       </Splash>
     </Container>
   );
