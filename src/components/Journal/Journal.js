5d28d331b2ece53048cf878f503cc9bd068fdb52
Tranquility Base <tfauvelle@gmail.com>
2022-07-09 12:04:29 -0400

    simple non styled journal component created

--- src/components/Journal/Journal.js
+++ src/components/Journal/Journal.js
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "../common/Form";
+import Input from "../common/Input";
+import InlineInputContainer from "../common/InlineInputContainer";
+import AdvButton from "../common/AdvButton";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+import TextArea from "../common/TextArea";
 
-const Journal = (props) => {
-  return <h1>journal</h1>;
+const Journal = ({ query, submitting, updateForm, onSubmit }) => {
+  const [value, setValue] = useState("");
+
+  const handleSelect = (e) => {
+    console.log(e);
+    setValue(e);
+  };
+
+  const handleSubmit = (e) => {
+    onSubmit(e);
+  };
+
+  const handleChange = (e) => {
+    updateForm(e.target.id, e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>journal</h1>
+      <div>
+        <DropdownButton
+          alignRight
+          title="Select Activity"
+          id="dropdown-menu-align-right"
+          onSelect={handleSelect}
+        >
+          <Dropdown.Item eventKey="Activity-1">Activity-1</Dropdown.Item>
+          <Dropdown.Item eventKey="Activity-2">Activity-2</Dropdown.Item>
+          <Dropdown.Item eventKey="Activity-3">Activity-3</Dropdown.Item>
+        </DropdownButton>
+        <h4>{value}</h4>
+        <Form
+          onSubmit={handleSubmit}
+          style={{ maxWidth: "800px", width: "100" }}
+        >
+          <InlineInputContainer>
+            <TextArea />
+          </InlineInputContainer>
+          <AdvButton
+            style={{ marginTop: "10px" }}
+            variant="primary"
+            size="lg"
+            type="submit"
+          >
+            Submit
+          </AdvButton>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Journal;
--- src/components/Profiles/Dog.js
+++ src/components/Profiles/Dog.js
@@ -1,14 +1,19 @@
 import React from "react";
 import BorderCard from "../common/BorderCard";
 import AdvButton from "../common/AdvButton";
+import { useNavigate } from "react-router-dom";
 
 const Dog = (props) => {
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate("/journalEntry");
+  };
   const { id, name, avatar, age, weight, sex, breed, deleted } = props.dog;
   return (
     <BorderCard>
       <h2>Name: {name}</h2>
       <AdvButton
-        onClick={() => props.onSelect(id)}
+        onClick={onClick}
         style={{
           width: "auto",
           color: "#F1F1F1",
