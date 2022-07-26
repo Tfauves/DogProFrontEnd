0de17e773601e10c7612d17ca07b2e8410ec29bc
Tranquility Base <tfauvelle@gmail.com>
2022-07-26 15:23:31 -0400

    deploy issuse test

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -35,7 +35,8 @@ const Register = () => {
     const data = query;
     data.name = query.fname + " " + query.lname;
     try {
-      const res = await axios.post(`${apiHost}/api/auth/signup`, data);
+      const host = process.env.REACT_APP_API_HOST || "http://localhost:8080";
+      const res = await axios.post(`${host}/api/auth/signup`, data);
 
       login(data);
     } catch (err) {
